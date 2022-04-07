34b5efb3ef7adef0bd44404d803d796be54e417b
ChenyangLEI <935747833@qq.com>
2022-04-07 12:32:24 +0000

    add mscoco

--- scripts/mscoco.sh
+++ scripts/mscoco.sh
@@ -8,11 +8,19 @@ MODEL=$ALGO-mixup$MIXUP-aug$AUG
 
 CUDA_VISIBLE_DEVICES=$GPU python pretrain.py exp.name=$DATA-$MODEL dataset=$DATA algorithm=$ALGO spatialaug=$AUG mixup=$MIXUP
 
+#mkdir /home/aiscuser/dabs/DATASETS/captioned_images/mscoco
+#cp mnt/input/data/coco_raw/*zip /home/aiscuser/dabs/DATASETS/captioned_images/mscoco
+#cd /home/aiscuser/dabs/DATASETS/captioned_images/mscoco
 
+#unzip test2017.zip
+#unzip train2017.zip
+#unzip val2017.zip
+#unzip annotations_trainval2017.zip
+#cd ~/dabs
 
 SAVE_DIR=/mnt/input/projects/dabs/models/
-cp $SAVE_DIR/$DATA-$MODEL/'epoch=4-step=99999.ckpt' $SAVE_DIR/$DATA-$MODEL/epoch_4-step_99999.ckpt
-CKPT=$SAVE_DIR/$DATA-$MODEL/epoch_4-step_99999.ckpt
+cp $SAVE_DIR/$DATA-$MODEL/'epoch=10-step=99999.ckpt' $SAVE_DIR/$DATA-$MODEL/epoch_10-step_99999.ckpt
+CKPT=$SAVE_DIR/$DATA-$MODEL/epoch_10-step_99999.ckpt
 echo Transfer $TRANSFER
 echo ckpt $CKPT
 
