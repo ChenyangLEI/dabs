11af6045fadd6d57b2478838fa5286652b3036a4
ChenyangLEI <935747833@qq.com>
2022-04-07 11:28:18 +0000

    mscoco

---
+++ scripts/mscoco.sh
@@ -0,0 +1,21 @@
+# python pretrain.py exp.name=<experiment-name> dataset=<dataset> algorithm=<algorithm>
+AUG=$AUG
+GPU=$GPU
+DATA=mscoco
+MIXUP=1
+ALGO=estyle
+MODEL=$ALGO-mixup$MIXUP-aug$AUG
+
+CUDA_VISIBLE_DEVICES=$GPU python pretrain.py exp.name=$DATA-$MODEL dataset=$DATA algorithm=$ALGO spatialaug=$AUG mixup=$MIXUP
+
+
+
+SAVE_DIR=/mnt/input/projects/dabs/models/
+cp $SAVE_DIR/$DATA-$MODEL/'epoch=4-step=99999.ckpt' $SAVE_DIR/$DATA-$MODEL/epoch_4-step_99999.ckpt
+CKPT=$SAVE_DIR/$DATA-$MODEL/epoch_4-step_99999.ckpt
+echo Transfer $TRANSFER
+echo ckpt $CKPT
+
+DATA2=traffic_sign
+TRANSFER=$DATA-to-$DATA2-$MODEL
+CUDA_VISIBLE_DEVICES=$GPU python transfer.py exp.name=$TRANSFER dataset=$DATA2 ckpt=$CKPT
