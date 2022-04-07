ffcaa057607e014dc37189ad2e0692afc9c51ea1
ChenyangLEI <935747833@qq.com>
2022-04-07 12:57:49 +0000

    mscoco

--- scripts/mscoco.sh
+++ scripts/mscoco.sh
@@ -5,10 +5,8 @@ MIXUP=1
 ALGO=estyle
 MODEL=$ALGO-mixup$MIXUP-aug$AUG
 
-CUDA_VISIBLE_DEVICES=$GPU python pretrain.py exp.name=$DATA-$MODEL dataset=$DATA algorithm=$ALGO spatialaug=$AUG mixup=$MIXUP
-
 mkdir /home/aiscuser/dabs/DATASETS/captioned_images/mscoco
-cp mnt/input/data/coco_raw/*zip /home/aiscuser/dabs/DATASETS/captioned_images/mscoco
+cp /mnt/input/data/coco_raw/*zip /home/aiscuser/dabs/DATASETS/captioned_images/mscoco
 cd /home/aiscuser/dabs/DATASETS/captioned_images/mscoco
 
 unzip test2017.zip
@@ -17,9 +15,12 @@ unzip val2017.zip
 unzip annotations_trainval2017.zip
 cd ~/dabs
 
+CUDA_VISIBLE_DEVICES=$GPU python pretrain.py exp.name=$DATA-$MODEL dataset=$DATA algorithm=$ALGO spatialaug=$AUG mixup=$MIXUP
+
+
 SAVE_DIR=/mnt/input/projects/dabs/models/
-cp $SAVE_DIR/$DATA-$MODEL/'epoch=10-step=99999.ckpt' $SAVE_DIR/$DATA-$MODEL/epoch_10-step_99999.ckpt
-CKPT=$SAVE_DIR/$DATA-$MODEL/epoch_10-step_99999.ckpt
+cp $SAVE_DIR/$DATA-$MODEL/'epoch=7-step=99999.ckpt' $SAVE_DIR/$DATA-$MODEL/epoch_7-step_99999.ckpt
+CKPT=$SAVE_DIR/$DATA-$MODEL/epoch_7-step_99999.ckpt
 echo Transfer $TRANSFER
 echo ckpt $CKPT
 
