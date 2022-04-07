a97e9c51fe8ef27aaa4e6dbccaa7a4c47446cf1e
Chenyang LEI <935747833@qq.com>
2022-04-07 14:12:23 +0800

    Update speech.sh

--- scripts/speech.sh
+++ scripts/speech.sh
@@ -7,13 +7,15 @@ MODEL=$ALGO-mixup$MIXUP-aug$AUG
 CUDA_VISIBLE_DEVICES=$GPU python -W ignore pretrain.py exp.name=$DATA-$MODEL dataset=$DATA algorithm=$ALGO spatialaug=$AUG mixup=$MIXUP
 echo MODEL $MODEL
 
-
-SAVE_DIR=$PWD/base_dir
+SAVE_DIR=/mnt/input/projects/dabs/models/
 cp $SAVE_DIR/$DATA-$MODEL/'epoch=22-step=99999.ckpt' $SAVE_DIR/$DATA-$MODEL/epoch_22-step_99999.ckpt
 CKPT=$SAVE_DIR/$DATA-$MODEL/epoch_22-step_99999.ckpt
 echo Transfer $TRANSFER
 echo ckpt $CKPT
 
+git clone 'https://github.com/soerenab/AudioMNIST'
+mkdir /home/aiscuser/dabs/DATASETS/speech
+mv AudioMNIST /home/aiscuser/dabs/DATASETS/speech/AudioMNIST-master
 DATA2=audio_mnist
 TRANSFER=$DATA-to-$DATA2-$MODEL
 CUDA_VISIBLE_DEVICES=$GPU python -W ignore transfer.py exp.name=$TRANSFER dataset=$DATA2 ckpt=$CKPT
