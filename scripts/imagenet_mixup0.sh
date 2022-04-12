1f475aa654a85eb847dad261898edfbbb47d4a15
Chenyang LEI <935747833@qq.com>
2022-04-12 19:18:57 +0800

    Update imagenet_mixup0.sh

--- scripts/imagenet_mixup0.sh
+++ scripts/imagenet_mixup0.sh
@@ -24,16 +24,33 @@ fi
 ALGO=estyle
 MODEL=$ALGO-mixup$MIXUP-aug$AUG-ratio$RATIO
 echo 'MODEL:'  $MODEL
+
 CUDA_VISIBLE_DEVICES=$GPU python pretrain.py exp.name=$DATA-$MODEL dataset=$DATA algorithm=$ALGO spatialaug=$AUG mixup=$MIXUP ratio=$RATIO
 
 
 
-DATA2=pamap2
-TRANSFER=$DATA-to-$DATA2-$MODEL
 SAVE_DIR=/mnt/input/projects/dabs/models/
-cp $SAVE_DIR/$DATA-$MODEL/'epoch=512-step=99999.ckpt' $SAVE_DIR/$DATA-$MODEL/epoch_512-step_99999.ckpt
-CKPT=$SAVE_DIR/$DATA-$MODEL/epoch_512-step_99999.ckpt
+cp $SAVE_DIR/$DATA-$MODEL/'epoch=4-step=99999.ckpt' $SAVE_DIR/$DATA-$MODEL/epoch_4-step_99999.ckpt
+CKPT=$SAVE_DIR/$DATA-$MODEL/epoch_4-step_99999.ckpt
 echo Transfer $TRANSFER
 echo ckpt $CKPT
 
+DATA2=traffic_sign
+TRANSFER=$DATA-to-$DATA2-$MODEL
+CUDA_VISIBLE_DEVICES=$GPU python transfer.py exp.name=$TRANSFER dataset=$DATA2 ckpt=$CKPT
+
+DATA2=dtd
+TRANSFER=$DATA-to-$DATA2-$MODEL
+CUDA_VISIBLE_DEVICES=$GPU python transfer.py exp.name=$TRANSFER dataset=$DATA2 ckpt=$CKPT
+
+DATA2=vgg_flower
+TRANSFER=$DATA-to-$DATA2-$MODEL
+CUDA_VISIBLE_DEVICES=$GPU python transfer.py exp.name=$TRANSFER dataset=$DATA2 ckpt=$CKPT
+
+DATA2=cifar10
+TRANSFER=$DATA-to-$DATA2-$MODEL
+CUDA_VISIBLE_DEVICES=$GPU python transfer.py exp.name=$TRANSFER dataset=$DATA2 ckpt=$CKPT
+
+DATA2=aircraft
+TRANSFER=$DATA-to-$DATA2-$MODEL
 CUDA_VISIBLE_DEVICES=$GPU python transfer.py exp.name=$TRANSFER dataset=$DATA2 ckpt=$CKPT
