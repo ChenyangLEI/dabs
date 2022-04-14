1b767401f0b95ead2599810dd926c360949fde97
Chenyang LEI <935747833@qq.com>
2022-04-14 12:58:58 +0800

    Update wiki.sh

--- scripts/wiki.sh
+++ scripts/wiki.sh
@@ -1,10 +1,37 @@
 DATA=wikitext103
-AUG=$AUG
-MIXUP=1
+if [ $AUG is '']
+then
+    AUG=1
+else
+    AUG=$AUG
+fi
+
+if [ $GPU is '']
+then
+    GPU=0
+else
+    GPU=$GPU
+fi
+
+if [ $RATIO is '']
+then
+    RATIO=1
+else
+    RATIO=$RATIO
+fi
+
+if [ $MIXUP is '']
+then
+    MIXUP=0
+else
+    MIXUP=$MIXUP
+fi
+
+
 ALGO=estyle
 MODEL=$ALGO-mixup$MIXUP-aug$AUG
-GPU=$GPU
-CUDA_VISIBLE_DEVICES=$GPU python pretrain.py exp.name=$DATA-$MODEL dataset=$DATA algorithm=$ALGO spatialaug=$AUG mixup=$MIXUP
+
+CUDA_VISIBLE_DEVICES=$GPU python pretrain.py exp.name=$DATA-$MODEL dataset=$DATA algorithm=$ALGO spatialaug=$AUG mixup=$MIXUP ratio=$RATIO
 
 
 
